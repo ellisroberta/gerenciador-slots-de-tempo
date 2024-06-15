82b8e5b4cde835d6ae0f36d717df6dd2dafc6164
ellisroberta <ellisroberta@hotmail.com>
2024-06-15 19:21:54 -0300

    Commit inicial

---
+++ src/main/java/com/example/gerenciador_slots_de_tempo/GerenciadorSlotsDeTempoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_slots_de_tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorSlotsDeTempoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorSlotsDeTempoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gerenciador_slots_de_tempo/GerenciadorSlotsDeTempoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_slots_de_tempo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorSlotsDeTempoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
